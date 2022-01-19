66e2b47ae839a51fdb8e536f357bc0dbdcc60c99
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-19 15:02:04 -0600

    Added checkSchemeId middleware

--- api/server.js
+++ api/server.js
@@ -7,4 +7,12 @@ const server = express();
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
+server.use('*', (req, res) => {
+    res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
+});
+
+server.use((error, req, res, next) => {
+    res.status(error.status || 500).json({ message: error.message });
+});
+
 module.exports = server;
